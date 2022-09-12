28fbde9f93baec8c4ff700c87a6b93c12d7351fb
leelasiva <104293726+leelasiva@users.noreply.github.com>
2022-09-12 14:51:02 +0530

    Add files via upload

---
+++ 01-birthday-reminder/setup/src/data.js
@@ -0,0 +1,42 @@
+import ammu from './assests/ammu.jpg';
+import honey from './assests/honey.jpg';
+import sunny from './assests/sunny.jpg';
+import lavanya from './assests/lavanya.jpg';
+import janu from './assests/janu.jpg';
+export default [
+  {
+    id: 1,
+    name: 'Ammu',
+    age: '07',
+    image:ammu,
+
+  },
+  {
+    id: 2,
+    name: 'Honey',
+    age: '22',
+    image:honey
+,
+  },
+  {
+    id: 3,
+    name: 'Sunny',
+    age: '20',
+    image:
+      sunny,
+  },
+  {
+    id: 4,
+    name: 'Janu',
+    age: '04',
+    image:
+janu,
+  },
+  {
+    id: 5,
+    name: 'Lavanya',
+    age: '35',
+    image:
+lavanya,
+  },
+];
